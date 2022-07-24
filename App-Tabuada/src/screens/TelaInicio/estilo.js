f2ccf3c76ddf82fef206d3b8f4dad72f74a1dba1
Davi Fernando Rosa <davir0514@gmail.com>
2022-07-24 15:13:36 -0300

    2022-07-24 version final

--- App-Tabuada/src/screens/TelaInicio/estilo.js
+++ App-Tabuada/src/screens/TelaInicio/estilo.js
@@ -1,63 +1,63 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 const estilo = StyleSheet.create({
   imagemFundo: {
-    width: '100%', 
-    height: '100%'
+    width: "100%",
+    height: "100%"
   },
 
-  tela: { 
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex:1
+  tela: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1
   },
 
   boxPergunta: {
-    backgroundColor: 'rgba(255,255,255,0.85)',
+    backgroundColor: "rgba(255,255,255,0.88)",
     borderRadius: 10,
     margin: 20,
     padding: 20,
     alignItems: "center",
     width: 320
   },
-  
+
   titulo: {
     marginBottom: 20,
-    paddingHorizontal: 10, 
+    paddingHorizontal: 10,
     fontSize: 26,
     textTransform: "uppercase",
     fontWeight: "700",
     textAlign: "center",
-    color:"#1f4f66"
+    color: "#1f4f66"
   },
 
   boxQuestao: {
-    flexDirection: 'row',
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center"
-  },  
+  },
 
   texto: {
     marginRight: 10,
     fontSize: 26
   },
-  
+
   input: {
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     borderRadius: 3,
     width: 100,
     paddingHorizontal: 5,
-    marginLeft: 10 ,
+    marginLeft: 10,
     fontSize: 26
   },
 
   opcoes: {
-    width:320,
+    width: 320,
     marginTop: 20,
-    alignItems:"center",
-    justifyContent: 'space-around',
-    flexDirection: 'row'
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row"
   },
 
   boxBotao: {
@@ -68,7 +68,6 @@ const estilo = StyleSheet.create({
   boxBotaoTabuada: {
     width: 320
   }
+})
 
-});
-
-export default estilo;
+export default estilo
